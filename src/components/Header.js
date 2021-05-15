36778181c84c25bcf6ee760311ba7e3ba36e62be
Sopee Thong <sthong@Sopees-MBP.attlocal.net>
2021-05-15 02:30:33 -0400

    adding header formatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import "../styles/Header.css";
+
+const styles = {
+    headerStyle: {
+      background: "lightblue"
+    },
+    headingStyle: {
+      fontSize: 50
+    }
+  };
+
+  const pStyles ={
+    headerStyle: {
+        background: "lightblue"
+      },
+      headingStyle: {
+        fontSize: 15,
+        fontColor: "black",
+        textAlign: "center",
+      }
+    };
 
 function Header() {
 	return (
-		<div className="header">
-			<h1>Employee Directory</h1>
-			<p>
+        <header style={styles.headerStyle} className="header">
+            <h1 style={styles.headingStyle}>Employee Directory </h1> 
+            <br></br>
+			<p style={pStyles.headingStyle}>
 				Click on carrots to filter by heading or use the search box to narrow
 				your results.
 			</p>
-		</div>
+            </header>
 	);
 }
 
