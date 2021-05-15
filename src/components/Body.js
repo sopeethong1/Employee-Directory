e4633be400d0bb8d893dc2f64f1dfac9053daaf9
Sopee Thong <sthong@Sopees-MBP.attlocal.net>
2021-05-15 00:58:52 -0400

    adding componentDidMount function

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,3 +1,32 @@
 import React from "react";
 import API from "../utils/API";
 
+export default class Body extends Component {
+	
+	state = {
+		users: [],
+		filteredUsers: [],
+	};
+
+	componentDidMount() {
+	
+		API.getUsers().then((res) => {
+		
+			const holdData = res.data.results.map((user) => {
+				return user;
+			});
+			this.setState({
+				users: holdData,
+			});
+			console.log(this.state.users);
+		});
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<Table users={this.state.users} />
+			</div>
+		);
+	}
+}
