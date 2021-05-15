71109ed9e6dd07ee67ee5ffa6edac5168f9d1502
Sopee Thong <sthong@Sopees-MBP.attlocal.net>
2021-05-15 00:22:53 -0400

    install axios

---
+++ src/utils/API.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+// Export an object containing methods we'll use for accessing the Wikipedia API
+
+export default {
+  searchTerms: function(query) {
+    return axios.get("https://randomuser.me/api/?results=5000");
+  },
+};
