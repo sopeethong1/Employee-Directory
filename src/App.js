e8f766f202bdf7ce4cafc52e20407fefeb173224
Sopee Thong <sthong@Sopees-MBP.attlocal.net>
2021-05-15 02:32:05 -0400

    removed icon import

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
-import Body from './components/Body';
-import Header from './components/Header';
+
+import Body from "./components/Body";
+import Header from "./components/Header";
+
 
 function App() {
-	return (
-		<div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-			<Header />
-			<Body />
-		</div>
-	);
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+    </div>
+  );
 }
 
 export default App;
