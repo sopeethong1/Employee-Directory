774614aca58b4e500072f07697206d10eeec9a82
Sopee Thong <sthong@Sopees-MBP.attlocal.net>
2021-05-15 02:32:54 -0400

    adding userAPI variable

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,8 +2,10 @@ import axios from "axios";
 
 // Export an object containing methods we'll use for accessing the Wikipedia API
 
+let userApi = "https://randomuser.me/api/?results=20"
+
 export default {
   getUsers: function() {
-    return axios.get("https://randomuser.me/api/?results=20");
+    return axios.get(userApi);
   },
 };
