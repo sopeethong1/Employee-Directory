a60a8488c9864a2380c93ed3666de38829788a63
Sopee Thong <sthong@Sopees-MBP.attlocal.net>
2021-05-15 00:31:04 -0400

    reducing results num

---
+++ src/components/Body.js
@@ -0,0 +1,3 @@
+import React from "react";
+import API from "../utils/API";
+
--- src/utils/API.js
+++ src/utils/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Export an object containing methods we'll use for accessing the Wikipedia API
 
 export default {
-  searchTerms: function(query) {
-    return axios.get("https://randomuser.me/api/?results=5000");
+  getUsers: function() {
+    return axios.get("https://randomuser.me/api/?results=20");
   },
 };
