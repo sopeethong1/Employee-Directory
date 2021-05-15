c0a9db452e09440a5071db56dc83e99567d16b66
Sopee Thong <sthong@Sopees-MBP.attlocal.net>
2021-05-15 03:12:43 -0400

    removing extra files

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,34 +1,30 @@
-import React, { Component } from 'react';
-import API from "../utils/API";
-import Table from './Table';
-
+import React, { Component } from "react";
+import API from "...../utils/API";
+import Table from "../Table/index";
 
 export default class Body extends Component {
-	
-	state = {
-		users: [],
-		filteredUsers: [],
-	};
+  state = {
+    users: [],
+    filteredUsers: [],
+  };
 
-	componentDidMount() {
-	
-		API.getUsers().then((res) => {
-		
-			const holdData = res.data.results.map((user) => {
-				return user;
-			});
-			this.setState({
-				users: holdData,
-			});
-			console.log(this.state.users);
-		});
-	}
+  componentDidMount() {
+    API.getUsers().then((res) => {
+      const holdData = res.data.results.map((user) => {
+        return user;
+      });
+      this.setState({
+        users: holdData,
+      });
+      console.log(this.state.users);
+    });
+  }
 
-	render() {
-		return (
-			<div className="container">
-				<Table users={this.state.users} />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="container">
+        <Table users={this.state.users} />
+      </div>
+    );
+  }
 }
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React from 'react';
 
 function Table(props) {
+    console.log(props);
   const userData = props.users.map((user) => {
     return <div>{user.name.first}</div>;
   });
-  console.log("This is the userData after returning as a div", userData);
-  return <div className="container"></div>;
+  console.log("User Data!", userData);
+  return (
+  <div className="container">  
+  {userData} 
+  
+  
+  
+  
+  </div>
+
+  );
 }
 
 export default Table;
